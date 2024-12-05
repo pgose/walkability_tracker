4021c7a4482ddc4f50c6ceca899d0119898206ab
pgose <philip.gst@protonmail.ch>
2024-12-05 16:22:12 +0100

    changed file structure to accommodate typical Flask syntax

---
+++ app.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+import pyproj
+
+from flask import Flask, jsonify, request, redirect, render_template, url_for
+from markupsafe import escape
+
+
+app = Flask(__name__, static_folder='static')
+
+@app.route("/")
+def hello_world():
+    return render_template('index.html')
+
+@app.route("/walk")
+def walk():
+    return render_template('walk.html')
+
+@app.route("/heatmap")
+def heatmap():
+    return render_template('heatmap.html')
+
+@app.route("/debug")
+def debug():
+    return render_template('debug.html')
+
+@app.route("/test")
+def test():
+    return "<h1>Test Pog</h1><p>This is merely a test :)</p>"
+
+
+app.run(debug=True, host="localhost", port=8989)
